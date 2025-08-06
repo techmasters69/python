830d5522dd040887b32016d37a2821376a6316cb
techmasters69 <m.hamzakamran999@gmail.com>
2025-08-06 17:37:21 +0700

    Add files via upload

---
+++ s.py
@@ -0,0 +1,12 @@
+name = "steve"
+age = "100"
+is_student = True
+weight = 9999999999999999999999
+print("name :", name)
+print("data type of name is", type(name))
+print("age :", age)
+print("data type of age is", type(age))
+print("is_student :", is_student)
+print("data type of is_student is", type(is_student))
+print("weight :", weight)
+print("data type of weight is", type(weight))
