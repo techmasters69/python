9d5ba8ed71b51d69e4e2fa03373852b6b5adcddc
techmasters69 <m.hamzakamran999@gmail.com>
2025-08-06 17:55:37 +0700

    Add files via upload

---
+++ o.py
@@ -0,0 +1,13 @@
+num1 = 45
+num2 = 3
+
+print("number1", num1)
+print("number2", num2)
+print("addition", num1+num2)
+print("difference :", num1+num2)
+print("product :", num1+num2)
+print("division :", num1+num2)
+print("floor division :", num1+num2)
+print("modulus operartion :", num1+num2)
+print("square :", num2**2)
+print("square :", num1**0.5)
