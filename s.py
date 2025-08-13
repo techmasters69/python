cb84b0036d1f924f4a02af08e05c3bfee564cb4c
techmasters69 <m.hamzakamran999@gmail.com>
2025-08-13 17:24:24 +0700

    Add files via upload

--- s.py
+++ s.py
@@ -1,12 +1,8 @@
-name = "steve"
-age = "100"
-is_student = True
-weight = 9999999999999999999999
-print("name :", name)
-print("data type of name is", type(name))
-print("age :", age)
-print("data type of age is", type(age))
-print("is_student :", is_student)
-print("data type of is_student is", type(is_student))
-print("weight :", weight)
-print("data type of weight is", type(weight))
+number = int(input("enter number to check"))
+print("number to be checked :", number)
+if number>0:
+    print("this is a positive")
+elif number<0:
+    print("this is a negitive")
+else:
+    print("this is nuutral")
