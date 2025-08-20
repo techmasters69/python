b46a40f500a8eff793ce3444684cd8ad7e6b78fa
techmasters69 <m.hamzakamran999@gmail.com>
2025-08-20 17:22:07 +0700

    Add files via upload

---
+++ somthing.py
@@ -0,0 +1,7 @@
+def absolute_value(num):
+    if num >= 0:
+        return num
+    else:
+        return -num
+print("Absolute value of 89 :", absolute_value(89))
+print("absolute value of -189:", absolute_value(-189))
